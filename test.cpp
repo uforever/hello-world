9a36caac7e78344129a9f181094b813724112362
uforever <38533204+uforever@users.noreply.github.com>
2018-06-28 02:09:29 +0800

    Update test.cpp
    add a line

--- test.cpp
+++ test.cpp
@@ -3,5 +3,6 @@
 int main(int argc, char *argv[])
 {
     std::cout << "Aloha World!" << std::endl;
+    std::cout << "The quieter you become, the more you are able to hear." << std::endl;
     return 0;
 }
